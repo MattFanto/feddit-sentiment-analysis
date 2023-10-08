33e88463ef0bb12f29cdce5c7d95a9403d23e264
matt <mattia.fantoni@gmail.com>
2023-10-08 22:57:10 +0100

    fixed test

--- tests/acceptance_tests/smoke_test.py
+++ tests/acceptance_tests/smoke_test.py
@@ -6,6 +6,6 @@ from reddit_sentiment_analysis.config import settings
 
 def test_predict_endpoint():
     """Smoke test to check deployment is working"""
-    response = requests.get(f"http://{settings.host}:{settings.port}/api/v1/predict?subfeddit_id=1")
+    response = requests.get(f"http://{settings.host}:{settings.port}/api/v1/detect-comments-sentiment?subfeddit_id=1")
     assert response.status_code == 200
     assert len(response.json()["data"]) > 0
--- tests/unit_test/service/test_predict.py
+++ tests/unit_test/service/test_predict.py
@@ -2,21 +2,26 @@ import boto3
 import moto
 import pytest
 
-from reddit_sentiment_analysis.services.predict import aws_predict_sentiment
+from reddit_sentiment_analysis.services.detect_sentiment import aws_detect_sentiment
+from reddit_sentiment_analysis.services.feddit_api import Comment
+
+
+def get_comment(text):
+    return Comment(text=text, id=0, username="test", created_at=0)
 
 
 def test_aws_predict_sentiment_success():
     with moto.mock_comprehend() as m:
-        comment = {"text": "This is a great day!"}
+        comment = get_comment("This is a great day!")
         client = boto3.client("comprehend")
-        response = client.detect_sentiment(Text=comment["text"], LanguageCode="en")
-        res = aws_predict_sentiment(comment)
+        response = client.detect_sentiment(Text=comment.text, LanguageCode="en")
+        res = aws_detect_sentiment(comment)
         assert res.score == response["SentimentScore"]["Positive"] - response["SentimentScore"]["Negative"]
         assert res.sentiment == response["Sentiment"]
 
 
 def test_aws_predict_empty_input():
     with moto.mock_comprehend() as m:
-        comment = {"text": ""}
+        comment = get_comment("")
         with pytest.raises(Exception):
-            aws_predict_sentiment(comment)
+            aws_detect_sentiment(comment)
